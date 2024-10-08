2ca0b4a199c0a0c0d4085807c73871ca98b888b4
ThobiasN <thobias.nordgaard@gmail.com>
2024-10-08 21:49:35 +0200

    fix circular symlink and relative dir

--- mklinks.sh
+++ mklinks.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-# get the original users home directory
+# Get the original user's home directory
 USER_HOME=$(eval echo ~$SUDO_USER)
 
-# automaticly symlink dotfiles to their respective places
+# Get the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Automatically symlink dotfiles to their respective places
 # nixos
-ln -sf $(realpath ./nixos/configuration.nix) /etc/nixos/configuration.nix
+sudo ln -sf "$SCRIPT_DIR/nixos/configuration.nix" /etc/nixos/configuration.nix
 
 # hyprland
-ln -sf $(realpath ./hypr/hyprland.conf) "$USER_HOME/.config/hypr/hyprland.conf"
+ln -sf "$SCRIPT_DIR/hypr/hyprland.conf" "$USER_HOME/.config/hypr/hyprland.conf"
 
 # rofi
-ln -sf $(realpath ./rofi/config.rasi) "$USER_HOME/.config/rofi/config.rasi"
+ln -sf "$SCRIPT_DIR/rofi/config.rasi" "$USER_HOME/.config/rofi/config.rasi"
 
-# nvim
-ln -sf $(realpath ./nvim) "$USER_HOME/.config/nvim"
+# nvim (remove existing directory or symlink if it exists, then create the symlink)
+rm -rf "$USER_HOME/.config/nvim"
+ln -sf "$SCRIPT_DIR/nvim" "$USER_HOME/.config/nvim"
