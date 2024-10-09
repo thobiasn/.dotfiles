1a7ad27db24858bd56ac644b9c85c241576879e6
ThobiasN <thobias.nordgaard@gmail.com>
2024-10-09 19:57:26 +0200

    add dunstrc

--- mklinks.sh
+++ mklinks.sh
@@ -14,9 +14,13 @@ sudo ln -sf "$SCRIPT_DIR/nixos/configuration.nix" /etc/nixos/configuration.nix
 ln -sf "$SCRIPT_DIR/hypr/hyprland.conf" "$USER_HOME/.config/hypr/hyprland.conf"
 ln -sf "$SCRIPT_DIR/hypr/start.sh" "$USER_HOME/.config/hypr/start.sh"
 
-# rofi
+# rofi app launcher
 ln -sf "$SCRIPT_DIR/rofi/config.rasi" "$USER_HOME/.config/rofi/config.rasi"
 
 # nvim (remove existing directory or symlink if it exists, then create the symlink)
 rm -rf "$USER_HOME/.config/nvim"
 ln -sf "$SCRIPT_DIR/nvim" "$USER_HOME/.config/nvim"
+
+# dunst notifications
+rm -rf "$USER_HOME/.config/dunst"
+ln -sf "$SCRIPT_DIR/dunst" "$USER_HOME/.config/dunst"
